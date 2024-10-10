37ec492121493207eca544af9d4bac98dc9952a5
Daniela478 <danielamarqu17@gmail.com>
2024-10-10 08:07:06 -0400

    Inicio del ejercicio

---
+++ js/Principal.js
@@ -0,0 +1,20 @@
+/*El promedio de notas resulta de dividir el acumulado de notas entre la cantidad de materias.
+Se tiene de todo estudiante su nombre, acumulado notas y cantidad materias.
+Un estudiante becado recibe una beca mensual de $30 si su promedio de notas es superior a
+15, ó recibe $20 si es al menos $10; si el promedio es menor de 10, no recibe beca.
+Diseñe las clases Cl_estudiante y Cl_becado, usando herencia, de manera que procese un
+estudiante y reporte el monto de la beca, o si no le corresponde beca.
+
+Ejemplos de la corrida:
+Nombre del estudiante: Mary
+Acumulado de notas: 132
+Cantidad de materias: 8
+Nota promedio: 16.50
+Mary recibe una beca de $30.00
+
+Nombre del estudiante: Alirio
+Acumulado de notas: 315
+Cantidad de materias: 35
+Nota promedio: 9.00
+Alirio no le corresponde beca */
+
