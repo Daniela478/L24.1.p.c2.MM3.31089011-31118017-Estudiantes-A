f56f2338703ed80118db109264d9f34523370324
Daniela478 <danielamarqu17@gmail.com>
2024-10-10 09:23:30 -0400

    Cambios

--- js/Cl_becado.js
+++ js/Cl_becado.js
@@ -1,12 +1,11 @@
-import Cl_Estudiante from "./Cl_Estudainte.js"
 export default class Cl_becado extends Cl_Estudiante {
 constructor (nombre, acumulado ,materias){
     super(nombre, acumulado ,materias)
 }
     recibe(){
-    if(this.promedio >= 15)
+    if(this.promedio() >= 15)
      return 30;
-    else if(this.promedio >=10) 
+    else if(this.promedio() >=10) 
         return 20;
     else 
         return "No le corresponde beca";
